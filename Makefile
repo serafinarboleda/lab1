679abd243cbc11bab34ab639aa562ece059820ca
root <root@laptop>
2023-01-29 22:54:31 -0800

    my first commit for lab-1

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: lab1
+
+lab1: lab1.cpp
+	g++ lab1.cpp -Wall -o lab1
+
+clean:
+	rm -f lab1
---
+++ lab1.cpp
@@ -0,0 +1,17 @@
+//Serafin Arboleda
+//lab-1 Jan 24
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{ 
+    // I made some changes in this file
+    // hopefully it works this time:     
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hello World\n";
+
+    return 0;
+    
+}
